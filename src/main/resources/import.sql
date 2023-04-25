b4aade2324658882dfe303032476ffaf08633464
Samuelmetaenlace <samuel.jimenez@metaenlace.com>
2023-04-25 10:11:33 +0200

    Initial commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+
+
+INSERT INTO employees (name, age, direction_id) VALUES ('employee1', 30, 1);
+INSERT INTO employees (name, age, direction_id) VALUES ('employee2', 30, 2);
+INSERT INTO employees (name, age) VALUES ('employee3', 30);
